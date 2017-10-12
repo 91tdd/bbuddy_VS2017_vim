23ac9df40b4f76ac363d1225132dfb0fc18e2961
hatelove <jay.hatelove@gmail.com>
2017-10-12 22:40:33 +0800

    add a failed test case: one day budget

--- BBuddy/Accounting.cs
+++ BBuddy/Accounting.cs
@@ -2,6 +2,13 @@
 {
     public class Accounting
     {
+        private readonly IBudgetRepo _budgetRepo;
+
+        public Accounting(IBudgetRepo budgetRepo)
+        {
+            _budgetRepo = budgetRepo;
+        }
+
         public decimal TotalAmount(string start, string end)
         {
             return 0;
--- BBuddy/UnitTest1.cs
+++ BBuddy/UnitTest1.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace BBuddy
 {
@@ -7,16 +9,30 @@ namespace BBuddy
     {
         private Accounting accounting;
 
+        private IBudgetRepo stubRepo = Substitute.For<IBudgetRepo>();
+
         [SetUp]
         public void Setup()
         {
-            accounting = new Accounting();
+            accounting = new Accounting(stubRepo);
         }
 
         [Test]
         public void no_budget()
         {
+            stubRepo.GetAll().Returns(new List<Budget>());
             TotalAmountShouldBe(0, "20170701", "20170701");
+        }
+
+        [Test]
+        public void one_day_budget()
+        {
+            stubRepo.GetAll().Returns(new List<Budget>()
+            {
+                new Budget{Month="201707",Amount=31}
+            });
+
+            TotalAmountShouldBe(1, "20170701", "20170701");
         }
 
         private void TotalAmountShouldBe(int expected, string start, string end)
@@ -24,5 +40,16 @@ namespace BBuddy
             decimal totalAmount = accounting.TotalAmount(start, end);
             Assert.AreEqual(expected, totalAmount);
         }
+    }
+
+    public interface IBudgetRepo
+    {
+        List<Budget> GetAll();
+    }
+
+    public class Budget
+    {
+        public int Amount { get; set; }
+        public string Month { get; set; }
     }
 }
