6a7dffdad37aebc26890de965e916bbfd4287374
hatelove <jay.hatelove@gmail.com>
2017-10-12 22:24:34 +0800

    add the first failed test case: no budgete

---
+++ BBuddy/Accounting.cs
@@ -0,0 +1,10 @@
+﻿namespace BBuddy
+{
+    public class Accounting
+    {
+        public decimal TotalAmount(string start, string end)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- BBuddy/UnitTest1.cs
+++ BBuddy/UnitTest1.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
 
 namespace BBuddy
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Test]
+        public void no_budget()
         {
+            Accounting accounting = new Accounting();
+            decimal totalAmount = accounting.TotalAmount("20170701", "20170701");
+            Assert.AreEqual(0, totalAmount);
         }
     }
 }
